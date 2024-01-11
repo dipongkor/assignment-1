0406d27ea60791a3f92a7a3d420a50ff6739846b
Atish Kumar Dipongkor <atish@Atishs-MBP.lan>
2024-01-11 11:49:21 -0500

    parseInt used to convert string to number

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import fibonacci from "./fib";
 
 export default (req: Request, res: Response) => {
-  const num = req.params.num as string;
+  const num = req.params.num;
 
   const fibN = fibonacci(parseInt(num));
   let result = `fibonacci(${num}) is ${fibN}`;
